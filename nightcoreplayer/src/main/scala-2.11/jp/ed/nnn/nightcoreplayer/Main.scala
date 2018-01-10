70b00a28c083efba546203041265628fe06188c2
nappannda <qwe159753qwee@gmail.com>
2018-01-10 19:16:13 +0900

    Finish 16
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # ♻️  :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

---
+++ nightcoreplayer/src/main/scala-2.11/jp/ed/nnn/nightcoreplayer/Main.scala
@@ -0,0 +1,37 @@
+package jp.ed.nnn.nightcoreplayer
+
+/**
+  * Created by nappannda on 2018/01/10.
+  */
+import java.io.File
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.layout.BorderPane
+import javafx.scene.media.{Media, MediaPlayer, MediaView}
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+
+object Main extends App {
+  Application.launch(classOf[Main], args: _*)
+}
+
+class Main extends Application {
+
+  override def start(primaryStage: Stage): Unit = {
+
+    val path = "/Users/nappannda/video.mp4"
+    val media = new Media(new File(path).toURI.toString)
+    val mediaPlayer = new MediaPlayer(media)
+    mediaPlayer.setRate(1.25)
+    mediaPlayer.play()
+    val mediaView = new MediaView(mediaPlayer)
+    val baseBorderPane = new BorderPane()
+    baseBorderPane.setStyle("-fx-background-color: Black")
+    baseBorderPane.setCenter(mediaView)
+    val scene = new Scene(baseBorderPane, 800, 500)
+    scene.setFill(Color.BLACK)
+    primaryStage.setScene(scene)
+    primaryStage.show()
+  }
+
+}
